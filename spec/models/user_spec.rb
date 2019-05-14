63e25e45c7707dae40d8d5f5694b827af66a014b
Kota Miyamoto <kota.miyamoto@leverages.jp>
2019-05-14 16:44:21 +0900

    testの書き方変えた

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,24 +4,24 @@ RSpec.describe User, type: :model do
 
     context "ユーザーが登録する時" do
         example "名前とメールアドレスとパスワードがあれば登録できる" do 
-            expect(FactoryBot.create(:user)).to be_valid
+            is_asserted_by { !build(:user) }
         end 
     
         example "名前がなければ登録できない" do 
-            expect(FactoryBot.build(:user, name: "")).to_not be_valid 
+            is_asserted_by { !build(:user, name: '') }
         end 
     
         example "メールアドレスがなければ登録できない" do 
-            expect(FactoryBot.build(:user, email: "")).to_not be_valid 
+            is_asserted_by { !build(:user, email: '') }
         end 
     
         example "メールアドレスが重複していたら登録できない" do 
             user1 = FactoryBot.create(:user,name: "taro", email: "taro@example.com")
-            expect(FactoryBot.build(:user, name: "ziro", email: user1.email)).to_not be_valid
+            is_asserted_by { !build(:user, name: 'ziro', email: user1.email) }
         end 
     
         example "パスワードがなければ登録できない" do 
-            expect(FactoryBot.build(:user, password: "")).to_not be_valid 
+            is_asserted_by { !build(:user, password: '') }
         end 
     
         example "パスワードが暗号化されているか" do 
@@ -30,7 +30,7 @@ RSpec.describe User, type: :model do
         end 
     
         example "password_confirmationとpasswordが異なる場合保存できない" do 
-            expect(FactoryBot.build(:user,password:"password",password_confirmation: "passward")).to_not be_valid 
+            is_asserted_by { !build(:user, password: "password", password_confirmation: "password") }
         end 
     end
 end
