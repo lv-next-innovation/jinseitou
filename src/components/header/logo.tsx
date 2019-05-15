d39c7ae858f63259437652c29d4230ca87ada8b6
takuya nakamatsu <uv.nkmt.yii@gmail.com>
2019-05-15 09:36:13 +0900

    修正完了。

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import * as Redux from 'redux';
 
-
 const Logo = (props: any) => (
     <div className="logo">
       人生灯だよ
     </div>
   );
 
-  export default Logo;
+export default Logo;
 
