084b824b49cd7d08aa68a9a3ff86ba737ad18a60
Kota Miyamoto <kota.miyamoto@leverages.jp>
2019-05-15 18:30:22 +0900

    ブログのシングルページのデータ取得

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -1,10 +1,18 @@
 module Api
   module V1
     class BlogsController < ApplicationController
+
       def index
-        blogs = Blog.order(created_at: :desc)
+        blogs = Blog.all
         render json: { status: 'SUCCESS', message: 'loaded blogs', data: blogs }
       end
+
+      def show
+        blog = Blog.find(params[:id])
+        render json: { status: 'SUCCESS', message: 'loaded single blog', data: blog}
+      end
+
+
     end
   end
 end
