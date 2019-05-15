7e82623a865213e2acbdc131468370c8f56caab2
t-kusakabe <ttmac0704@gmail.com>
2019-05-16 02:03:39 +0900

    test通す

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -3,14 +3,54 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before do
-    @user = build(:user)
-  end
-
   describe 'バリデーション' do
-    it 'emailが空だとNG' do
-      @user.email = ''
-      expect(@user.valid?).to eq(false)
+
+    context 'emailが空' do
+      let(:user) { build :user, email: '' }
+
+      example '永続化できない' do
+        is_asserted_by { !user.email.nil? }
+        is_asserted_by { !user.valid? }
+      end
+    end
+
+    context '名前とメールアドレスとパスワードがあれば登録できる' do
+      let(:user) { build :user }
+
+      example '永続化できる' do
+        is_asserted_by { user.valid? }
+      end
+    end
+
+    context '名前がなければ登録できない' do
+      let(:user) { build :user, name: '' }
+      example '永続化できない' do
+        is_asserted_by { !user.valid? }
+      end
+    end
+
+    context 'メールアドレスがなければ登録できない' do
+      let(:user) { build :user, email: '' }
+      example '永続化できない' do
+        is_asserted_by { !user.valid? }
+      end
+    end
+
+    context 'メールアドレスが重複していたら登録できない' do
+      let!(:user) { create :user, email: 'Kota@kota-inc.com' }
+      let(:duplicate_user) { build :user, email: 'Kota@kota-inc.com' }
+
+      example '永続化できない' do
+        is_asserted_by { !duplicate_user.valid? }
+      end
+    end
+
+    context 'パスワードがなければ登録できない' do
+      let(:user) { build :user, password: '' }
+
+      example '永続化できない' do
+        is_asserted_by { !user.valid? }
+      end
     end
   end
 end
