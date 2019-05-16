fa7dee0d8c8458f1bc2c50b669da8145539f71e3
takuya nakamatsu <uv.nkmt.yii@gmail.com>
2019-05-16 10:24:49 +0900

    メインコンポーネント内にブログの内容リストとなるDetailコンポーネントを作成し、追加。

---
+++ src/components/blog/detail.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+
+const Detail = (props: any) =>(
+  <div className="detail">
+    sample text
+  </div>
+);
+
+export default Detail;
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
+import Detail from './blog/detail';
 
 const Main = (props: any) => (
   <div className="main">
-    sample
+    <Detail />
   </div>
 );
 
