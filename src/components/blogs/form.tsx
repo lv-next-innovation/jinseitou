c1f66be1949145e7ba47777bfa50556aaa57a6ac
t-kusakabe <ttmac0704@gmail.com>
2019-05-17 07:33:17 +0900

    ひとまずtemplateにビジネスロジック置いておく

--- src/components/blogs/form.tsx
+++ src/components/blogs/form.tsx
@@ -1,18 +1,26 @@
 import * as React from "react";
+import axios from 'axios';
+
+const sendBlog = () => {
+  axios.post('/api/v1/blogs', {
+    message: 'foo',
+    user_id: 1
+  }).then((res) => {
+    console.log(res.data);
+  })
+};
 
 const Form = (props: any) => (
   <div className="blog__form">
     <h3>フォーム</h3>
-    <form>
       <label htmlFor="blog__form__content">内容</label>
-      <br />
+
       <input
         type="text"
         name="blog__form__content"
         placeholder="入力してください"
       />
-      <input type="submit" value="送信" />
-    </form>
+      <button onClick={() => }>Submit</button>
   </div>
 );
 
