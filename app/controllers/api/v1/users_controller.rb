363e7480c8bd57fd3d6210cda0f458ceff5851e5
原田将貴 <marurunnmakesgames@gmail.com>
2019-05-16 17:28:31 +0900

    userコントローラのdestroy機能追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,10 +4,18 @@ module Api
 	module V1
 		class UsersController < ApplicationController
 
+			protect_from_forgery
+
 			def show
 				user = User.find(params[:id])
 				render json: { status: 'SUCCESS', user: user }
 			end
+
+			def destroy
+        user = User.find(params[:id])
+        user.destroy
+        render json: { status: 'SUCCESS' }
+      end
 
 		end
 	end
