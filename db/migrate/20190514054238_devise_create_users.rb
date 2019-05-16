85ce99acf9668cdd91193f67114a1e8df4917c56
原田将貴 <marurunnmakesgames@gmail.com>
2019-05-16 16:27:23 +0900

    【修正】userテーブルのカラムの並び順を変更しました

--- db/migrate/20190514054238_devise_create_users.rb
+++ db/migrate/20190514054238_devise_create_users.rb
@@ -3,12 +3,12 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :name
       t.string :email,              null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
       t.datetime :remember_created_at
-      t.string :name
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_001840) do
+ActiveRecord::Schema.define(version: 2019_05_15_095431) do
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "question", default: "", null: false
@@ -20,7 +20,15 @@ ActiveRecord::Schema.define(version: 2019_05_15_001840) do
   end
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "message", default: "", null: false
+    t.string "content", default: "", null: false
+    t.integer "user_id"
+    t.datetime "published_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "histories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content", default: "", null: false
     t.integer "user_id"
     t.datetime "published_at"
     t.datetime "created_at", null: false
@@ -28,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_05_15_001840) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
