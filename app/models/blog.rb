cbe26faf62cd3bdf627b4ee1ac37c9306792d9d8
Kota Miyamoto <kota.miyamoto@leverages.jp>
2019-05-16 11:00:46 +0900

    add destroy update

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Blog < ApplicationRecord
-  validates :content, presence: true
+  validates :message, presence: true
   validates :user_id, presence: true
 end
