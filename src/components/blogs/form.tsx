b674f78b146bb8a67b47c002f700ecfc04547003
takuya nakamatsu <uv.nkmt.yii@gmail.com>
2019-05-16 12:07:28 +0900

    tslint,prettierを通しました。       e node_modules:./node_modules

--- src/components/blogs/form.tsx
+++ src/components/blogs/form.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 
-const Form = (props: any) =>(
+const Form = (props: any) => (
   <div className="blog__form">
     <h3>フォーム</h3>
     <form>
       <label htmlFor="blog__form__content">内容</label>
-      <br/>
-      <input type="text" name="blog__form__content" placeholder="入力してください" />
-      <input type="submit" value="送信"></input>
+      <br />
+      <input
+        type="text"
+        name="blog__form__content"
+        placeholder="入力してください"
+      />
+      <input type="submit" value="送信" />
     </form>
   </div>
 );
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import BlogsDetail from "./blogs/detail";
-import Form from './blogs/form';
+import Form from "./blogs/form";
 
 const Main = (props: any) => (
   <div className="main">
