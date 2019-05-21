8d6f3f5fcc48a20c8f4acb2ece5b1e58f2182257
t-kusakabe <ttmac0704@gmail.com>
2019-05-21 14:54:13 +0900

    ロゴを表示できるようにする

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 import * as Redux from "redux";
 
-const Logo = (props: any) => <div className="logo">人生灯だよ</div>;
+const Logo = (props: any) => (
+  <div className="header__logo">
+    <img src="./logo.png" className="header__logo__img" />
+  </div>
+);
 
 export default Logo;
