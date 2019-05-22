07e3b02362287a9f92c7945ae44533a1a711abf1
原田将貴 <marurunnmakesgames@gmail.com>
2019-05-22 11:08:18 +0900

    rubcopに怒られないように修正した

--- app/controllers/api/v1/itos_controller.rb
+++ app/controllers/api/v1/itos_controller.rb
@@ -1,27 +1,26 @@
 # frozen_string_literal: true
 
 module Api
-    module V1
-      class ItosController < ApplicationController
+  module V1
+    class ItosController < ApplicationController
+      protect_from_forgery
 
-        protect_from_forgery
+      def index
+        itos = Ito.all
+        render json: { status: 'SUCCESS', message: 'loaded Itos', data: itos }
+      end
 
-        def index
-          itos = Ito.all
-          render json: { status: 'SUCCESS', message: 'loaded Itos', data: itos }
-        end
+      def create
+        ito = Ito.create(ito_params)
 
-        def create
-          ito = Ito.create(ito_params)
-          
-          render json: { status: 'SUCCESS', itos: ito }
-        end
+        render json: { status: 'SUCCESS', itos: ito }
+      end
 
-        private
+      private
 
-        def ito_params
-            params.permit(:target, :thread)
-        end
+      def ito_params
+        params.permit(:target, :thread)
       end
     end
   end
+end
--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -4,8 +4,8 @@ module Api
   module V1
     class QuestionsController < ApplicationController
       def show
-        yml = YAML.load_file("config/question.yml")
-        
+        yml = YAML.load_file('config/question.yml')
+
         render json: { status: 'SUCCESS', question: yml.sample }
       end
     end
