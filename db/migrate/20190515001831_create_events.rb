43365d9cd36f446cb10d96ca36eea18ea47c5b16
原田将貴 <marurunnmakesgames@gmail.com>
2019-05-23 10:59:43 +0900

    default値のNOWをなくしました

--- db/migrate/20190515001831_create_events.rb
+++ db/migrate/20190515001831_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
       t.string :title, null: false, default: ''
-      t.datetime :event_date, null: false, default: -> { "NOW()" }
+      t.datetime :event_date, null: false
       t.integer :user_id
       t.timestamps
     end
