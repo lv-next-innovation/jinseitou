709b99a48e35d36e7d4da2582f014f59674ba6a9
t-kusakabe <ttmac0704@gmail.com>
2019-05-24 14:43:15 +0900

    データを追加

--- src/components/charts/map.tsx
+++ src/components/charts/map.tsx
@@ -4,18 +4,53 @@ import { Bubble } from "react-chartjs-2";
 const data = {
   datasets: [
     {
-      backgroundcolor: "rgba(225,95,150, 1)",
+      backgroundColor: "RGBA(225,95,150, 1)",
       data: [
         { x: 50, y: 75 },
       ],
-      label: "高校生の時"
+      label: "高校2生の時"
     },
     {
-      backgroundcolor: "rgba(115,255,25, 1)",
+      backgroundColor: "RGBA(115,255,25, 1)",
       data: [
         { x: 62, y: 79 },
       ],
       label: "Good & Moreの時"
+    },
+    {
+      backgroundColor: "RGBA(115,102,89, 1)",
+      data: [
+        { x: 24, y: 53 },
+      ],
+      label: "中学3年生の時"
+    },
+    {
+      backgroundColor: "RGBA(100,128,248, 1)",
+      data: [
+        { x: 90, y: 56 },
+      ],
+      label: "新卒交流会"
+    },
+    {
+      backgroundColor: "RGBA(180,199,58, 1)",
+      data: [
+        { x: 4, y: 48 },
+      ],
+      label: "習い事を始めさせてくれた時"
+    },
+    {
+      backgroundColor: "RGBA(104,18,157, 1)",
+      data: [
+        { x: 43, y: 38 },
+      ],
+      label: "大学3年生"
+    },
+    {
+      backgroundColor: "RGBA(104,18,157, 1)",
+      data: [
+        { x: 82, y: 24 },
+      ],
+      label: "彼女に振られた時"
     }
   ]
 };
